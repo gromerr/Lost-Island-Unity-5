014dcf480ba81d6ed701e6a5f4626a30774a8528
Grom <gromerr86@gmail.com>
2015-05-04 21:30:57 +0200

    Add options to main menu and exit from application

--- Lost-Island/Assets/Scripts/MenuBehaviour.cs
+++ Lost-Island/Assets/Scripts/MenuBehaviour.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class MenuBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void ExitFromGame(){
+		Application.Quit();
 	}
 }
